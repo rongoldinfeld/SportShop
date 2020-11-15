bce74f84e4077519369504dfeaee40551c2f09fb
Ron Goldinfeld <rongoldinfeld56@gmail.com>
2020-11-15 22:56:09 +0200

    removed logger from AboutUs controller

--- SportShop/Controllers/AboutUsController.cs
+++ SportShop/Controllers/AboutUsController.cs
@@ -1,17 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace SportShop.Controllers
 {
     public class AboutUsController : Controller
     {
-        private readonly ILogger<AboutUsController> _logger;
-
-        public AboutUsController(ILogger<AboutUsController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
